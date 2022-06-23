6fa485fa06378e4c3086c338d93ee0aed41ab654
Yoon <yoondev93@gmail.com>
2022-06-23 18:21:12 +0900

    서블릿 - HTTP 요청 데이터 > API 메시지 바디 > 단순 텍스트 - HTTP 요청 데이터 > API 메시지 바디 > JSON

---
+++ src/main/java/com/spring/servlet/basic/request/HelloData.java
@@ -0,0 +1,13 @@
+package com.spring.servlet.basic.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class HelloData {
+
+  private String username;
+  private int age;
+
+}
