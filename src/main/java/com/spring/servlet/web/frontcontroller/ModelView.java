be9651af13119b1f0accb7abadc7f59814a43c68
Yoon <yoondev93@gmail.com>
2022-07-03 19:24:21 +0900

    feature: Model 추가 - v3

---
+++ src/main/java/com/spring/servlet/web/frontcontroller/ModelView.java
@@ -0,0 +1,19 @@
+package com.spring.servlet.web.frontcontroller;
+
+import java.util.HashMap;
+import java.util.Map;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ModelView {
+
+  private String viewName;
+
+  private Map<String, Object> model = new HashMap<>();
+
+  public ModelView(String viewName) {
+    this.viewName = viewName;
+  }
+}
---
+++ src/main/java/com/spring/servlet/web/frontcontroller/v3/ControllerV3.java
@@ -0,0 +1,9 @@
+package com.spring.servlet.web.frontcontroller.v3;
+
+import com.spring.servlet.web.frontcontroller.ModelView;
+import java.util.Map;
+
+public interface ControllerV3 {
+
+  ModelView process(Map<String, String> paramMap);
+}
