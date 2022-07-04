38f303956a5f7a07ad77e2811f20271144d6c598
Yoon <yoondev93@gmail.com>
2022-07-04 15:32:40 +0900

    refactor: 패키지 그룹 변경

--- src/main/java/com/spring/servlet/web/frontcontroller/v3/ControllerV3.java
+++
@@ -1,9 +0,0 @@
-package com.spring.servlet.web.frontcontroller.v3;
-
-import com.spring.servlet.web.frontcontroller.ModelView;
-import java.util.Map;
-
-public interface ControllerV3 {
-
-  ModelView process(Map<String, String> paramMap);
-}
